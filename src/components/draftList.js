0d1fa4dac0b0a361c38fd8efacaa022052eddd0d
devin <devinmunasinghe@gmail.com>
2021-09-28 16:15:27 +0530

    draft list mobile UI List

--- src/components/draftList.js
+++ src/components/draftList.js
@@ -1,13 +1,95 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, ScrollView, TouchableOpacity } from 'react-native'
+
+
+const DATA = [
+    {
+        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item',
+    },
+    {
+        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+        title: 'Second Item',
+    },
+    {
+        id: '58694a0f-3da1-471f-bd96-145571e29d72',
+        title: 'Third Item',
+    },
+];
+
+const Item = ({ title }) => (
+    <TouchableOpacity>
+        <View style={[styles.itemS, styles.elevation]}>
+            <Text style={styles.title}>{title}</Text>
+        </View>
+    </TouchableOpacity>
+);
+
+
+
 
 function draftList() {
+
+    const renderItem = ({ item }) => (
+
+        <Item title={item.title} />
+
+
+
+    );
+
+
+
+
+
     return (
 
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>drafts!</Text>
-        </View>
+        <ScrollView>
+
+            <View style={{ flex: 1 }}>
+
+                <FlatList
+                    data={DATA}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                />
+
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+
+    itemS: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        backgroundColor: "white",
+        borderRadius: 10,
+
+        marginLeft: 17,
+        borderColor: "#D8D8D8",
+        borderWidth: 0.5
+
+    },
+    elevation: {
+        shadowColor: "#52006A",
+        elevation: 5,
+    },
+});
 
 export default draftList
